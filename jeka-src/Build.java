0470baf2f9958af5e8bf8186ccb2a26117071236
djeang <djeangdev@yahoo.fr>
2024-04-21 01:48:12 +0200

    sonarqube+coverage on java and js

--- jeka-src/Build.java
+++ jeka-src/Build.java
@@ -1,22 +1,11 @@
-/*
- * Copyright 2014-2024  the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *       https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- *  limitations under the License.
- */
-
 import dev.jeka.core.api.project.JkProject;
+import dev.jeka.core.api.system.JkProperties;
 import dev.jeka.core.tool.KBean;
 import dev.jeka.core.tool.builtins.project.ProjectKBean;
+import dev.jeka.plugins.nodejs.JkNodeJs;
+import dev.jeka.plugins.nodejs.NodeJsKBean;
+import dev.jeka.plugins.sonarqube.JkSonarqube;
+import dev.jeka.plugins.sonarqube.SonarqubeKBean;
 
 class Build extends KBean {
 
@@ -29,6 +18,25 @@ class Build extends KBean {
     @Override
     protected void init() {
         // configure project instance here
+    }
+
+    public void sonarJs() {
+        JkSonarqube javaSonarqube = load(SonarqubeKBean.class).sonarqube;
+        JkSonarqube jsSonarqube = javaSonarqube.copyWithoutProperties();
+        String projectId = project.getBaseDir().toAbsolutePath().getFileName() + "-js";
+        jsSonarqube
+                .setProperty(JkSonarqube.PROJECT_KEY, projectId)
+                .setProperty(JkSonarqube.PROJECT_NAME, projectId)
+                .setProperty(JkSonarqube.HOST_URL, getRunbase().getProperties().get("sonar.host.url"))
+                .setProperty(JkSonarqube.TOKEN, getRunbase().getProperties().get("sonar.token"))
+                .setProperty(JkSonarqube.PROJECT_BASE_DIR, getBaseDir().resolve("app-js").toString())
+                .setProperty(JkSonarqube.SOURCES, "src")
+                .setProperty(JkSonarqube.EXCLUSIONS, "dist/**/*, node_modules/**/*")
+                .setProperty(JkSonarqube.SOURCE_ENCODING, "UTF-8")
+                .setProperty(JkSonarqube.TEST, "src")
+                .setProperty(JkSonarqube.TEST_INCLUSIONS, "**/*.spec.ts")
+                .setProperty("sonar.typescript.lcov.reportPaths", "coverage/client/lcov.info");
+        jsSonarqube.run();
     }
 
 }
